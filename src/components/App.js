183fb62076cc0235261ca2c4c2970f855be1ba57
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-05-01 12:04:49 -0700

    Feat: Added backbones to components. Setting up file structure.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from "./Nav"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav/>
+      </header>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Quote extends Component {
+  render(){
+    return(
+      <div>Quote!</div>
+    )
+  }
+}
+
+export default Quote;
