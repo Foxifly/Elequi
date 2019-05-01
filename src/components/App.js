6ca4a142fda5f787291b66e9dd9238a7b63c1c9d
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-05-01 13:22:26 -0700

    Feat:   Router and NavLink implementation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Nav from "./Nav"
+import Quote from "./Quote";
+import Contact from "./Contact";
+import Landing from "./Landing";
+import About from "./About";
+import Services from "./Services";
+
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav/>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/quote" component={Quote} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/404" component={Quote} />
+
+
       </header>
 
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class Nav extends Component {
-  render(){
-    return(
-      <div>Hello!</div>
-    )
+  render() {
+    return (
+      <nav>
+        <ul>
+          <NavLink
+            className="nav-item-logo"
+            to="/"
+            exact
+            activeClassName="active"
+          >
+            <img alt="elequi pineapple logo" src="" />
+          </NavLink>
+
+          <NavLink className="nav-item" to="/" exact activeClassName="active">
+            Home
+          </NavLink>
+
+          <NavLink
+            className="nav-item"
+            to="/about"
+            exact
+            activeClassName="active"
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            className="nav-item"
+            to="/services"
+            exact
+            activeClassName="active"
+          >
+            Services
+          </NavLink>
+
+          <NavLink
+            className="nav-item"
+            to="/contact"
+            exact
+            activeClassName="active"
+          >
+            Contact Us
+          </NavLink>
+
+          <NavLink className="nav-item-quote" to="/quote" exact>
+            Get Quote
+          </NavLink>
+          
+        </ul>
+      </nav>
+    );
   }
 }
 
