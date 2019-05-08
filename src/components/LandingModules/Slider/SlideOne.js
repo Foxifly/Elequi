718c114d478ef331d24c18892bba473b77093b20
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-05-08 12:11:40 -0700

    Feat: Slide style and edits

--- src/components/LandingModules/Slider/SlideOne.js
+++ src/components/LandingModules/Slider/SlideOne.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react'
+import "../../../styles/landing/slideone.scss"
+/* REPLACE LOGO WITH CLEARER LOGO BUT THIS ONE IS TEMPORARY*/
+const ElequiPineapple = require('../../../img/Elequi-Pineapple.png')
+const ElequiLetters = require('../../../img/Elequi-letters.svg')
 
 class SlideOne extends Component {
   render(){
     return(
       <div className="slide-one">
-        <p className="slide-text">Elequi - Web design services</p>
+      <div className="logo-container">
+      <img className="elequi-pineapple" src={ElequiPineapple}/>
+      </div>
+
+      <div className="slide-one-text">
+        <img src={ElequiLetters} className="main-slide"/>
+        <p className="slide-text-main">Web design and development services.</p>
+        </div>
+
       </div>
     )
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/nav.scss";
+const ElequiLogo = require('../img/Elequi-Word-Logo.svg')
 
 class Nav extends Component {
   render() {
     return (
       <nav>
         <div className="logo-container">
-          <NavLink className="nav-item" to="/" exact activeClassName="active">
-            <img alt="elequi pineapple logo" src="" />
+          <NavLink className="nav-item" to="/" exact>
+            <img className="elequi-logo" alt="elequi pineapple logo" src={ElequiLogo} />
           </NavLink>
         </div>
         <div className="item-container">
@@ -62,7 +63,6 @@ class Nav extends Component {
             <div className="underline-effect" />
           </div>
           </div>
-
 
         <div className="button-container">
           <button className="quote-button">Get Quote</button>
